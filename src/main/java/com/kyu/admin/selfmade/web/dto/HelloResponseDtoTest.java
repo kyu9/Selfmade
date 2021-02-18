2148bd7d3b9bb85c20b72edaf5b1bf198f4d0ecf
kyu9 <kyudo97@naver.com>
2021-02-19 00:49:19 +0900

    fir Test + dto(-ing)

---
+++ src/main/java/com/kyu/admin/selfmade/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,18 @@
+package com.kyu.admin.selfmade.web.dto;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void lm_fn_test(){
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
