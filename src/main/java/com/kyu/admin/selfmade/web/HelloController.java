3745964507ca8acc71dc98e9a2a8099481f35d3b
kyu9 <kyudo97@naver.com>
2021-02-19 23:53:32 +0900

    DTO, dtoTest

--- src/main/java/com/kyu/admin/selfmade/web/HelloController.java
+++ src/main/java/com/kyu/admin/selfmade/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.kyu.admin.selfmade.web;
 
+import com.kyu.admin.selfmade.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //컨트롤러는 JSON을 반환하는 컨트롤러로 만들어줌
@@ -10,5 +12,11 @@ public class HelloController {
     @GetMapping("/test")
     public String hello(){
         return "Hello World!!";
+    }
+
+    @GetMapping("/test/dto")
+    //@RequestParam을 사용하게되면 API에서 넘긴 name이랑 amount 값을 가져와서 String name과 int amount에 넣어주는 과정을 거치게됨
+    public HelloResponseDto testDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/com/kyu/admin/selfmade/web/HelloControllerTest.java
+++ src/test/java/com/kyu/admin/selfmade/web/HelloControllerTest.java
@@ -7,14 +7,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 //테스트를 진행할 때 JUnit에 내장된 실행자 말고 SpringRunner라는 스프링 실행자를 사용
 //springboottest와 JUnit사이의 연결자 역할 수행
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest
 public class HelloControllerTest {
 
     //웹 API를 테스트할 떄 사용, 스프링 MVC 테스트의 시작
@@ -32,5 +32,21 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())
                 //Controller에서 Hello World을 리턴하기 떄문에 맞는지 확인
                 .andExpect(content().string(test));
+    }
+
+    @Test
+    public void testDto가_리턴되네() throws Exception{
+        String name = "hello";
+        int amount = 10000;
+
+        mvc.perform(get("/test/dto")
+                //param은 API를 테스트할 때 사용될 요청 파라미터를 설정하는 단계, String타입만 허용되기 떄문에 문자열로 변경해야함
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                //JSON응답값을 필드별로 검증 할 수 있는 메소드
+                //$.필드명 으로 사용
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
